5dadbd7c88a63688975a18c5a9d21a60fdc7d729
gildoong <lsy1507@gmail.com>
2025-11-07 02:22:21 +0000

    fix: add STATIC_ROOT setting

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIRS = [
 # 로그인/로그아웃 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
